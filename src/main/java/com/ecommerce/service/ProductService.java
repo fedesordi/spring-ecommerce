c9b3eb403be9c8b4246d70a136e5fff6f40a67ff
fedesordi <fedesordi23@gmail.com>
2024-12-26 15:42:20 -0300

    repository, service classes and product controller

---
+++ src/main/java/com/ecommerce/service/ProductService.java
@@ -0,0 +1,12 @@
+package com.ecommerce.service;
+
+import com.ecommerce.model.Product;
+
+import java.util.Optional;
+
+public interface ProductService {
+    public Product save(Product product);
+    public Optional<Product> get(Integer id);
+    public void update(Product product);
+    public void delete(Integer id);
+}
---
+++ src/main/java/com/ecommerce/service/ProductServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ecommerce.service;
+
+import com.ecommerce.model.Product;
+import com.ecommerce.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+    @Autowired
+    private ProductRepository productRepository;
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Optional<Product> get(Integer id) {
+        return productRepository.findById(id);
+    }
+
+    @Override
+    public void update(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.deleteById(id);
+    }
+}
