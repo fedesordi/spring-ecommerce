dd531a0d51d3b995c0752279b39ca7938e1c3752
fedesordi <fedesordi23@gmail.com>
2025-04-10 21:51:00 -0300

    ¿implemented image saving for the products

--- src/main/java/com/ecommerce/controller/ProductController.java
+++ src/main/java/com/ecommerce/controller/ProductController.java
@@ -3,15 +3,15 @@ package com.ecommerce.controller;
 import com.ecommerce.model.Product;
 import com.ecommerce.model.User;
 import com.ecommerce.service.ProductService;
+import com.ecommerce.service.UploadFileService;
 import org.slf4j.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.Optional;
 
 @Controller
@@ -22,6 +22,8 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private UploadFileService upload;
 
     @GetMapping
     public String show(Model model){
@@ -35,11 +37,28 @@ public class ProductController {
     }
 
     @PostMapping("/save")
-    public String save(Product product){
+    public String save(Product product, @RequestParam("img") MultipartFile file) throws IOException {
         LOGGER.info("This is the object product{}",product);
         User u = new User(1,"","","","","","","");
         product.setUser(u);
         productService.save(product);
+
+        //image
+
+        if(product.getId() == null){ //when the product is created
+            String imageName = upload.saveImage(file);
+            product.setImage(imageName);
+        } else {
+            if(file.isEmpty()){ //when the product is edited but the image isn't
+                    Product p = new Product();
+                    p = productService.get(product.getId()).get();
+                    product.setImage(p.getImage());
+            } else{
+                String imageName = upload.saveImage(file);
+                product.setImage(imageName);
+            }
+        }
+
         return "redirect:/products";
     }
     @GetMapping("/edit/{id}")
